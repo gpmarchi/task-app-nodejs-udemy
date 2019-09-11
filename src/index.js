e6237a545f936d1eb0b7810f439fc05d04628792
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-11 15:47:38 -0300

    patch and delete methods and preparing for routers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ require("./db/mongoose");
 
 const User = require("./models/user");
 const Task = require("./models/task");
+const userRouter = require("./routers/user");
 
 const log = console.log;
 
@@ -12,6 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(userRouter);
 
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -42,6 +44,59 @@ app.get("/users/:id", async (req, res) => {
 
   try {
     const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(400).send({ error: "Invalid user id provided!" });
+  }
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = Object.keys(User.prototype.schema.obj);
+  const isValidUpdate = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(400).send({ error: "Invalid user id provided!" });
+  }
+
+  try {
+    const user = await User.findByIdAndDelete(_id);
 
     if (!user) {
       return res.status(404).send();
@@ -90,6 +145,59 @@ app.get("/tasks/:id", async (req, res) => {
     res.send(task);
   } catch (error) {
     log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(400).send({ error: "Invalid task id provided!" });
+  }
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = Object.keys(Task.prototype.schema.obj);
+  const isValidUpdate = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(400).send({ error: "Invalid user id provided!" });
+  }
+
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
     res.status(500).send(error);
   }
 });
