71b86f495654d708f91e5473bc260db4f89e0a66
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-13 18:51:18 -0300

    restricting prop access get/del related tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("../models/task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -51,6 +52,12 @@ const userSchema = new mongoose.Schema({
   ]
 });
 
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner"
+});
+
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "thisismypass");
@@ -59,6 +66,14 @@ userSchema.methods.generateAuthToken = async function() {
   await user.save();
 
   return token;
+};
+
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userRawObject = user.toObject();
+  delete userRawObject.password;
+  delete userRawObject.tokens;
+  return userRawObject;
 };
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -85,6 +100,13 @@ userSchema.pre("save", async function(next) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre("remove", async function(next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
