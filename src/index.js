bbdae23a64189a0ec28ce40a45c2a3ed1ba19dab
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-18 18:07:10 -0300

    refactoring express config app.js to use in tests

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
+const app = require("./app");
 
-const log = console.log;
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
-app.disable("x-powered-by");
 
 app.listen(port, () => {
   log(`Server is up on ${port}`);
