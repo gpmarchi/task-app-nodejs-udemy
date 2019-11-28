1b009fd3cd97cb2e6fe366be15c0ffcb757bdecd
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-11-28 17:14:52 -0300

    configure cors in express for external api access

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
 const authRouter = require("./routers/auth");
 const userRouter = require("./routers/user");
@@ -10,6 +11,7 @@ const projectRouter = require("./routers/project");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(authRouter);
 app.use(userRouter);
 app.use(avatarRouter);
